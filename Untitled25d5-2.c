73277e79b30b848c684aa43463927fa085dae7ef
joaopedroteles <80984493+joaopedroteles@users.noreply.github.com>
2022-06-02 19:01:14 -0300

    Update Untitled25d5-2.c

--- Untitled25d5-2.c
+++ Untitled25d5-2.c
@@ -51,6 +51,9 @@ if(idade==82)
 {
 printf(" 82 anos ganha 1 premio \n ");
 }
+else{
+	printf(" idade invalida \n ");
+}
 
 }
 // -------------------------------
@@ -225,39 +228,34 @@ int main(){
 
 /*
 #include <stdio.h>
-#include <conio.h>
+ 
+ 
+void head();
+float soma(float n1, float num2);
+char le_valida_operador();
  
 int main() {
  
-	float n1,n2;
-	char op, continuar = 's';
-	int i = 0,  tam = 0;
+	float n1,n2,result;
+	char op;
  
- 	do{
-	 	printf("Deseja calcular (s/n)?");
-		scanf(" %c",&continuar);
-		
-	printf("---------------\n");
-	printf("CALC\n");
-	printf("---------------\n");
+	head(); 
  
 	printf("N1: ");
 	scanf("%f",&n1);
  
 	printf("OP:");
-	scanf(" %c",&op); 
+	op = le_valida_operador();
  
 	printf("N2: ");
 	scanf("%f",&n2);
  
- 
 	printf("---------------\n");
- 
- 
  
 	switch(op){
 		case '+':
-			printf("RESULTADO:   %.1f\n",n1+n2);
+			result = soma(n1,n2);
+			printf("RESULTADO:   %.1f\n",result); 
 		break;
 		case '-':
 			printf("RESULTADO:   %.1f\n",n1-n2);
@@ -268,19 +266,35 @@ int main() {
 		case '/':
 			printf("RESULTADO:   %.1f\n",n1/n2);
 		break;						
-
+ 
 		default:
 			printf("OPERADOR INVALIDO");
-		}
-		
-	}while(i<10 && continuar=='s');
-	
-	char le_valida_operador(){
+ 
+	}
+ 
+	return 0;
+}
+ 
+
+void head(){
+	printf("---------------\n");
+	printf("CALC\n");
+	printf("---------------\n");	
+}
+ 
+
+float soma(float n1, float num2){
+	float resultado;
+	resultado = n1 + num2;
+	return resultado;
+}
+ 
+ 
+char le_valida_operador(){
 	char operador;
 	int cont=0;
 	do{
 		if(cont!=0){
-			textcolor(4);
 			printf("-------\nERRO\n-------\n");
 		}
 		scanf(" %c",&operador);	
@@ -288,8 +302,4 @@ int main() {
 	}while(operador!='+' && operador!='-' && operador!='*' && operador!='/');	
  
 	return operador;
-}
-
- 
-	return 0;
 }*/
